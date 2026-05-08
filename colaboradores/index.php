076991aaf68952f600ccf626f83cef7d72a88c52
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2026-05-08 09:44:59 -0300

    feat(index.php): adiciona link para o favicon na página de colaboradores

--- colaboradores/index.php
+++ colaboradores/index.php
@@ -34,6 +34,7 @@ if ($busca) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Colaboradores - Sistema de Gestão</title>
+    <link rel="icon" href="../img/Favicon/Favicon%20Main/favicon.ico">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
