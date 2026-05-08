fcd55b554ca4ae412a17663caa4cc708a43b56d2
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2026-05-08 09:43:01 -0300

    feat(index.php): adiciona link para o favicon na página principal

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ foreach ($equipamentos as $equipamento) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Gestão</title>
+    <link rel="icon" href="img/Favicon/Favicon%20Main/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
