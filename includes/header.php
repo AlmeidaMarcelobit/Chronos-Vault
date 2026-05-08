4b376a3b1b69165eb382ddeb1b47e4ab16b009cc
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2026-05-08 09:48:28 -0300

    feat(header.php): adiciona link para o favicon no cabeçalho da página

--- includes/header.php
+++ includes/header.php
@@ -35,6 +35,7 @@ $active_equipamentos = (strpos($_SERVER['PHP_SELF'], 'equipamentos/') !== false)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Gestão</title>
+    <link rel="icon" href="../img/Favicon/Favicon%20Main/favicon.ico">
     <link rel="stylesheet" href="<?php echo $base_path; ?>css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
