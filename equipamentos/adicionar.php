71b558e726f0deaea53b1f6936c5fc299faaaa27
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2026-05-11 15:54:52 -0300

    feat(adicionar): adiciona estilos CSS para a página de adicionar equipamento

--- equipamentos/adicionar.php
+++ equipamentos/adicionar.php
@@ -117,7 +117,7 @@ if ($colaboradores === false) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Equipamento - Sistema de Gestão</title>
-    <link rel="stylesheet" href="../css/equipamentos.css">
+    <link rel="stylesheet" href="../css/equipamentos/adicionar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
