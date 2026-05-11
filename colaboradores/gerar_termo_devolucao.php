9870d628f358546bdb0519cb9e319f7cd144b352
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2026-05-11 13:18:41 -0300

    feat(termo_devolucao): cria novo estilo CSS específico e remove o antigo para o termo de devolução de equipamentos

--- colaboradores/gerar_termo_devolucao.php
+++ colaboradores/gerar_termo_devolucao.php
@@ -54,7 +54,7 @@ header('Content-Type: text/html; charset=UTF-8');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Termo de Devolução de Equipamento</title>
-    <link rel="stylesheet" href="../css/termos.css">
+    <link rel="stylesheet" href="../css/colaboradores/gerar_termo_devolucao.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
