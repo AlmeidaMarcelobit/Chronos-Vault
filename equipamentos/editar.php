14da0041c1d88de62df3c045790e19e5ad734fa6
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2026-05-11 16:38:55 -0300

    feat(estilo): adiciona estilos para a página de edição de equipamentos e atualiza o link do CSS no arquivo PHP correspondente

--- equipamentos/editar.php
+++ equipamentos/editar.php
@@ -203,7 +203,7 @@ $historicoCentroCusto = $equipamento['historico_centro_custo'] ?? [];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Equipamento - Sistema de Gestão</title>
-    <link rel="stylesheet" href="../css/equipamentos.css">
+    <link rel="stylesheet" href="../css/equipamentos/editar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
